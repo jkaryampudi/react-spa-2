7f49df8fc1cbf90be321707f6fa9b681b181a29b
Jay Karyampudi <71486788+jkaryampudi@users.noreply.github.com>
2025-02-02 16:27:36 +1100

    Update msalConfig.js

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -2,8 +2,8 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const MSAL_CONFIG = {
     auth: {
-        /*clientId: "8838d638-ab81-4618-a2f3-8b599deac91d",*/
-        clientId: "3e7276ba-2bdf-47b6-a5eb-4917c55727b8",
+        clientId: "8838d638-ab81-4618-a2f3-8b599deac91d",
+        /*clientId: "3e7276ba-2bdf-47b6-a5eb-4917c55727b8",*/
         authority: "https://login.microsoftonline.com/d3ad2e7f-49fe-4b21-88e1-58b4700e6a3b", // Tenant-specific
         redirectUri: "https://salmon-stone-00ae7060f.4.azurestaticapps.net",
     },
