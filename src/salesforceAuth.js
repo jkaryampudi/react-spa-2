9edec733388a2361612f8b43319833159f654c49
Jay Karyampudi <71486788+jkaryampudi@users.noreply.github.com>
2025-02-02 17:16:06 +1100

    Update salesforceAuth.js

--- src/salesforceAuth.js
+++ src/salesforceAuth.js
@@ -9,6 +9,7 @@ export const exchangeAzureTokenForSalesforce = async (azureToken) => {
     try {
         const response = await fetch(SALESFORCE_TOKEN_URL, {
             method: "POST",
+            mode: "no-cors",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: new URLSearchParams({
                 grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
