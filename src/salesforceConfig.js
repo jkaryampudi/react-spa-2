7c912fcc761331a1a400c9d85c9243f5c48bb04c
Jay Karyampudi <71486788+jkaryampudi@users.noreply.github.com>
2025-03-03 23:48:04 +1100

    Update salesforceConfig.js

--- src/salesforceConfig.js
+++ src/salesforceConfig.js
@@ -2,7 +2,7 @@ export const SALESFORCE_CONFIG = {
   clientId: "e5c9d570-3d8e-47c6-9f2c-ded134328a55", // From Azure AD B2C App
   clientSecret: "-uW8Q~V1U4zHLttgfUOudwlAfSsUCtBPB~L.jc59", // From Azure AD B2C App
   redirectUri: "https://gray-stone-07c20f810.6.azurestaticapps.net",
-  authUrl: "https://SFOLSOrg.b2clogin.com/SFOLSOrg.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SignIn",
+  authUrl: "https://SFOLSOrg.b2clogin.com/SFOLSOrg.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SFReact",
   tokenUrl: "https://SFOLSOrg.b2clogin.com/SFOLSOrg.onmicrosoft.com/oauth2/v2.0/token",
   experienceCloudUrl: "https://storm-eb707416e288b6.my.site.com/TESTCUST", // Replace with your Experience Cloud URL
 };
